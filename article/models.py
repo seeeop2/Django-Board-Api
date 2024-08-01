281e1a94766de73c714d5cd1c5b08565258d32df
INSEOP <seeeop2@gmail.com>
2024-08-01 19:52:15 +0900

    Feat: Article 모델 추가
    - Article 모델을 생성하여 게시글 정보를 관리하도록 함.
      * article_id: 게시글 ID (Primary Key)
      * subject: 게시글 제목 (최대 100자)
      * content: 게시글 내용
      * name: 사용자 이름 (최대 30자)
      * view_cnt: 조회 수 (기본값 0)
      * reg_date: 등록일 (자동으로 현재 시간 설정)

--- article/models.py
+++ article/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    # primary_key 없을 시, 자동으로 id 필드 생성
+
+    # 게시글 ID (PK)
+    article_id = models.AutoField(primary_key=True)
+    # 게시글 제목
+    subject = models.CharField(max_length=100)
+    # 게시글 내용
+    content = models.TextField()
+    # 사용자 이름
+    name = models.CharField(max_length=30)
+    # 조회 수
+    view_cnt = models.IntegerField(default=0)
+    # 등록일
+    reg_date = models.DateTimeField(auto_now_add=True)
